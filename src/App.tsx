1cd8f571ab253e493625f0129a2578580210d00c
Eurico Alves <euricojsalves@gmail.com>
2026-03-15 18:55:02 +0000

    feat: add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Feature1 from './features/feature-1';
+import Feature2 from './features/feature-2';
+import Feature3 from './features/feature-3';
+
+const App = () => {
+  return (
+    <div className="app">
+     h1>Gestor de Tarefas</h1>
+      
+      
+      
+    </div>
+  );
+};
+
+export default App;
